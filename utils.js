9f4edab4a2071764d869de7be66ab04280ff8411
Slippage <valardohaeris7x7@gmail.com>
2026-03-05 22:21:45 +0300

    Fix memory leak in event handlers

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T19:20:54.679Z
-Commit: Fix CSS styling bug on mobile devices
+Timestamp: 2026-03-05T19:21:45.568Z
+Commit: Fix memory leak in event handlers
