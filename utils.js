8c9b05bbe48650ce3be2b23b163158614a0f45b9
Slippage <valardohaeris7x7@gmail.com>
2026-03-05 22:16:02 +0300

    Fix broken links in documentation

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T19:11:51.656Z
-Commit: Implement user profile customization
+Timestamp: 2026-03-05T19:16:02.328Z
+Commit: Fix broken links in documentation
