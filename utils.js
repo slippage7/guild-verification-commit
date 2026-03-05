160f338e7ff77d442671ee33c601c02e693e3528
Slippage <valardohaeris7x7@gmail.com>
2026-03-05 22:37:13 +0300

    Fix broken links in documentation

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T19:34:24.857Z
-Commit: Fix race condition in async operations
+Timestamp: 2026-03-05T19:37:13.552Z
+Commit: Fix broken links in documentation
