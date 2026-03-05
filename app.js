ad01519dbc756fc2c4ce52226a3db1632b97de1b
Slippage <valardohaeris7x7@gmail.com>
2026-03-05 22:33:23 +0300

    Resolve null pointer exception in user service

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T19:32:58.951Z
-Commit: Add export functionality for reports
+Timestamp: 2026-03-05T19:33:23.467Z
+Commit: Resolve null pointer exception in user service
