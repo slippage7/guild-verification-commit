e6be44b8a226b9bba58efeca49f2fe5fea59bcf8
Slippage <valardohaeris7x7@gmail.com>
2026-03-05 22:20:54 +0300

    Fix CSS styling bug on mobile devices

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T19:20:17.608Z
-Commit: Add export functionality for reports
+Timestamp: 2026-03-05T19:20:54.679Z
+Commit: Fix CSS styling bug on mobile devices
