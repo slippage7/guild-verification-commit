3ee9eca95a6d79225b623c702db9adc5d6664da1
Slippage <valardohaeris7x7@gmail.com>
2026-03-16 17:32:04 +0300

    Resolve timezone issues in date handling

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-15T15:35:06.089Z
-Commit: Implement user profile customization
+Timestamp: 2026-03-16T14:32:04.106Z
+Commit: Resolve timezone issues in date handling
