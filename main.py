3a404e259243604482573e82c01a8ef2adac498e
Slippage <valardohaeris7x7@gmail.com>
2026-03-28 09:43:52 +0300

    Resolve null pointer exception in user service

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-27T05:53:04.875Z
-Commit: Fix CSS styling bug on mobile devices
+Timestamp: 2026-03-28T06:43:51.603Z
+Commit: Resolve null pointer exception in user service
