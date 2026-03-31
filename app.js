dc60bc5dc915eda7a840c67e6c04ff5ae7af81d9
Slippage <valardohaeris7x7@gmail.com>
2026-03-31 11:55:40 +0300

    Fix broken links in documentation

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-26T04:54:19.829Z
-Commit: Patch security vulnerability in authentication
+Timestamp: 2026-03-31T08:55:39.841Z
+Commit: Fix broken links in documentation
